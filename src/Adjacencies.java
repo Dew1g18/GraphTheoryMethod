fa5090d6520be48de6b30a9c1fff9671184badd2
Dew1g18 <Davewadd3@gmail.com>
2020-11-20 23:59:59 +0000

    I would like to do this at some point, along with a 3cycle builder too so that all that needs putting into this is the Adjacency matrix like in the graph theory package of maple. It is not however done yet

---
+++ src/Adjacencies.java
@@ -0,0 +1,18 @@
+public class Adjacencies {
+
+    Integer adjacencies[][];
+
+    public Adjacencies(int m[][]){
+        adjacencies = new Integer[m.length-1][m.length-1];
+        for (int i=0; i<m.length; i++){
+            Integer row[] ={};
+            for (int j=0; j<m.length;j++){
+                if (m[i][j]==1){
+
+                }
+            }
+        }
+
+    }
+
+}
